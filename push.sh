7d623dd6a8c01c5ead8a055c73c9dd6686d6c521
yanghao <283403891@qq.com>
2019-05-03 22:09:51 +0800

    master update.

--- push.sh
+++ push.sh
@@ -1,19 +1,22 @@
-# git checkout -b $1
-
-## add user file
-
-git add .
-
-git commit -m "branch $1"
-
-git push origin $1:$1
-
-git checkout master
-
-
-
 ### usage ###
+##
 ## git checkout -b xxxx
 ## (add user file)
 ## ./push.sh xxx
-### usage ###
+##
+### usage end ###
+
+git add .
+
+if [ "$1" ]; then
+  echo "$1"
+  git commit -m "branch $1"
+  git push origin $1:$1
+  git checkout master
+
+else
+  git commit -m "master update."
+  git push -u origin master
+
+fi
+
